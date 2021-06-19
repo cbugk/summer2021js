cf91f13981490cd76730afc349b6cd9f1e071a88
Celil Bugra Karacan <49951195+cbugra@users.noreply.github.com>
2021-06-19 21:06:46 +0000

    Exercise-1-3 implemented.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './exercises/1-2/App';
+import App from './exercises/1-3/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
